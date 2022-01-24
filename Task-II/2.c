d3cfc85bb104af9d5bd19c04246c99f573ec00f8
Šarūnas <86420873+6ar8nas@users.noreply.github.com>
2022-01-24 13:00:21 +0200

    Update 2.c

--- Task-II/2.c
+++ Task-II/2.c
@@ -1,7 +1,3 @@
-// Task 2 of the C Procedural Programming course practical work.
-// Enter an integer N. Enter N integers that will form a vector. Find the greatest distance (measured by positions of the 
-// elements) between the extremes of this vector. The program should output the element numbers and the distance between them.
-
 #include <stdio.h>
 
 void clearSymbols()

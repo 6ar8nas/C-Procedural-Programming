16c8886a40d1b84817933ab2e5a5d9e5475b474a
Šarūnas <86420873+6ar8nas@users.noreply.github.com>
2022-01-24 12:57:49 +0200

    Update 1.c

--- Task-I/1.c
+++ Task-I/1.c
@@ -1,6 +1,3 @@
-// Task 1 of the C Procedural Programming course practical work.
-// Enter a sequence of integers, which would end in 0. Output the amount of numbers, which had odd number of digits.
-
 #include <stdio.h>
 int main()
 {
